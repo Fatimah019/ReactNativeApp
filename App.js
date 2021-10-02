b0347fc5b67be8b1d5a1c10c83d39a5f95d5c2b3
fatimahdavies <f.davies@hazonholdings.com>
2021-10-02 19:29:43 +0100

    my first react native app to increment and decrement counts

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0)
+  const [inputVal, setInputVal] = useState("")
+
+  const handleIncrement= ()=>{
+    setCount(count + Number(inputVal))
+  }
+
+  const handleDecrement=()=>{
+    setCount(count - Number(inputVal))
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>FreeStyling</Text>
+      <View style={styles.inputAndResult}>
+        <TextInput style={styles.textInput} name="num" placeholder="0" keyboardType="numeric" value={inputVal.toString()} onChangeText={(e) => setInputVal(e)}/>
+        <Text style={styles.result}>{count}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Increment" onPress={()=> handleIncrement()}/>
+        <Button title="Decrement" onPress={()=> handleDecrement()}/>
+      </View>
     </View>
   );
 }
@@ -14,8 +30,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ccaa',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading:{
+    fontWeight: "bold",
+    fontSize: 30
+  },
+  inputAndResult:{
+    marginTop: 30,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  textInput:{
+    borderColor: "#ccac",
+    borderWidth: 3,
+    padding: 15,
+    width: 250
+  },
+  result:{
+    padding: 10,
+  },
+  buttonContainer:{
+    marginTop: 30,
+    width:"80%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  }
 });
---
+++ components/login.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Text>Login Here</Text>
+      <input type="text" placeholder="uername"/>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ccaa',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
